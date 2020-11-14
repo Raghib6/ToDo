27977180ef8192c14372c6c0e821fea4b4d00df0
Raghib <raghibshahriar8@gmail.com>
2020-11-14 23:20:45 +0600

    Created template successfully

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,5 +3,7 @@ from django.conf import settings
 from todo_app import views
 
 urlpatterns = [
-    
+    path('',views.homeVu,name='homepage'),
+    path('register/',views.signupVu,name='register'),
+    path('login/',views.loginVu,name='login')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homeVu(request):
+    context = {
+        'title' : 'Home',
+    }
+    return render(request,'index.html',context)
+
+def signupVu(request):
+    context = {
+        'title' : 'Sign Up',
+    }
+    return render(request,'signup.html',context)
+
+def loginVu(request):
+    context = {
+        'title' : 'Login',
+    }
+    return render(request,'login.html',context)
